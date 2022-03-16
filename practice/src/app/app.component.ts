9c1e328903a7fbc84665db0bffe50896af62c139
angiegigishang <shangxingyu@163.com>
2022-03-17 00:27:09 +0800

    add

---
+++ practice/src/app/app.component.ts
@@ -0,0 +1,104 @@
+import { Component } from '@angular/core';
+
+interface TopMenu {
+  title: string;
+  readonly link: string;
+}
+
+interface AddFunc {
+  (x: number, y: number): number; 
+}
+
+interface Dict {
+  [key: string]: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-practice';
+  menus: TopMenu[] = [
+    {
+      title: '热门',
+      link: ''
+    },
+    {
+      title: '男装',
+      link: ''
+    },
+    {
+      title: '百货',
+      link: ''
+    },
+    {
+      title: '运动',
+      link: ''
+    },
+    {
+      title: '手机',
+      link: ''
+    },
+    {
+      title: '家纺',
+      link: ''
+    },
+    {
+      title: '食品',
+      link: ''
+    },
+    {
+      title: '电器',
+      link: ''
+    },
+    {
+      title: '鞋包',
+      link: ''
+    },
+    {
+      title: '汽车',
+      link: ''
+    },
+    {
+      title: '水果',
+      link: ''
+    },
+    {
+      title: '电脑',
+      link: ''
+    },
+    {
+      title: '内衣',
+      link: ''
+    },
+    {
+      title: '家装',
+      link: ''
+    },
+    {
+      title: '母婴',
+      link: ''
+    },
+    {
+      title: '美妆',
+      link: ''
+    },
+    {
+      title: '家具',
+      link: ''
+    }
+  ];
+
+  add: AddFunc = (x, y) => x + y;
+
+  dict: Dict = {
+    a: '1',
+    b: '2'
+  }
+
+  constructor() {
+     console.log(this.dict['a'])
+  }
+}
